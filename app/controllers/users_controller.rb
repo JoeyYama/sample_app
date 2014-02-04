e0bdc005678ca92817d50be48493d5ce81cbe9df
Joey Yamanouchi <kffweopfifpwf@yahoo.com>
2014-02-04 16:55:47 -0600

    Some example implementations ch. 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,11 @@ class UsersController < ApplicationController
   end   
   
   def new
-    @user = User.new
+    #if :signed_in_user
+	#  redirect_to root_url
+	#else
+      @user = User.new
+	#end
   end
   
   def edit
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,17 +8,23 @@ describe "Authentication" do
     before { visit signin_path }
 
     describe "with invalid information" do
+	  let(:user) { FactoryGirl.create(:user) }
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
+	  it { should_not have_link('Users',       href: users_path) }
+	  it { should_not have_link('Profile',     href: user_path(user)) }
+	  it { should_not have_link('Settings',    href: edit_user_path(user)) }
+	  it { should_not have_link('Sign out',    href: signout_path) }
+	  it { should have_link('Sign in', href: signin_path) }
 	  
 	  describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
-	
+	      
 	describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
@@ -45,9 +51,10 @@ describe "Authentication" do
 	  describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+		  sign_in user
+          #fill_in "Email",    with: user.email
+          #fill_in "Password", with: user.password
+          #click_button "Sign in"
         end
 
         describe "after signing in" do
